a716301049c391f0614998710f81076736ff8914
Josevlz <jose.vlzlara@gmail.com>
2016-02-13 02:48:42 +1200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
+var X_Step =50;
+var Y_Step=50;
+
 // Enemies our player must avoid
+
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x=0;
+    this.y=20;
+    this.width = 50;
+    this.height = 85;
+    this.speed=Math.floor(Math.randon()*2);
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +22,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x =
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,11 +33,35 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player= function(){
+    this.sprite = 'images/char-boy.png';
+    this.x=200;
+    this.y=400;
+    
+};
+Player.prototype.update=funtion(dt){
+    if (player.y<20){
+        player.reset();
+    }
+     console.log('player x = ', this.x, 'player y = ', this.y);
+};
+Player.protoype.handleInput=funtion(e){
+    this.ctlKey=e;
+    
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies=[];
+function placeEnemy(){
+var enemy =new Enemy();
+enemy.x=X_step*2;
+enemy.y=Y_step*2;
+allEnemies.push(enemy);
+}
+var player =new Player();
 
 
 
